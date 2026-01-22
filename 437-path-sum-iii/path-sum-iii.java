afb9eb5c0b40e2ae2e9ce8de456912545933d613
dibya upadhyay <dibyaupadhyay2000@gmail.com>
2026-01-22 18:39:51 +0530

    Time: 3 ms (97.40%) | Memory: 46.4 MB (43.04%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        Map<Long, Integer> map=new HashMap<>();
+        map.put(0L, 1);
+        
+        return dfs(root, 0, targetSum, map);
+    }
+
+    private int dfs(TreeNode node, long currSum, int target, Map<Long, Integer>map){
+        if(node==null){
+            return 0;
+        }
+        currSum+=node.val;
+
+        int count=map.getOrDefault(currSum-target,0);
+
+        map.put(currSum, map.getOrDefault(currSum,0)+1);
+
+        count+=dfs(node.left, currSum, target, map);
+        count+=dfs(node.right, currSum, target, map);
+
+        map.put(currSum, map.get(currSum)-1);
+        return count;
+    }
+}
