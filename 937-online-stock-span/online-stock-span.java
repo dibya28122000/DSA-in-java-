9ec3c2e69f759e103da01c338085ca3112c915a5
dibya upadhyay <dibyaupadhyay2000@gmail.com>
2026-03-02 19:17:39 +0530

    Time: 30 ms (72.43%) | Memory: 54.4 MB (75.88%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class StockSpanner {
+    
+    private Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        
+        // Pop all smaller or equal prices
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+        
+        // Push current price and its span
+        stack.push(new int[]{price, span});
+        
+        return span;
+    }
+}
